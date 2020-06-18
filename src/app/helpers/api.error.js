1509793f54725b3da734d49a67c8654d3b81b185
LeonardoRamos <leoSLRamos@gmail.com>
2020-06-18 14:23:37 -0300

    Update api.error.js

--- src/app/helpers/api.error.js
+++ src/app/helpers/api.error.js
@@ -3,11 +3,13 @@ import httpStatus from 'http-status';
 class ExtendableError extends Error {
     constructor(message, status, isPublic) {
         super(message);
+        
         this.name = this.constructor.name;
         this.message = message;
         this.status = status;
         this.isPublic = isPublic;
         this.isOperational = true;
+        
         Error.captureStackTrace(this, this.constructor.name);
     }
 }
