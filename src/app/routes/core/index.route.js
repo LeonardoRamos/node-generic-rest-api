bf778de3a52d24616a17b6e740c6c1f318ce2a97
LeonardoRamos <leoslramos@gmail.com>
2020-06-30 14:01:54 -0300

    Improve health response

--- src/app/routes/core/index.route.js
+++ src/app/routes/core/index.route.js
@@ -3,7 +3,11 @@ import authRoutes from '../auth.route';
 
 const router = express.Router(); 
 
-router.get('/manage/health', (req, res) => res.send('OK'));
+router.get('/manage/health', (req, res) => res.send({
+    status: 'UP',
+    uptime: process.uptime(),
+    timestamp: Date.now()
+}));
 
 router.use('/auth', authRoutes);
 
