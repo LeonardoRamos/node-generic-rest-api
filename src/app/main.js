ac81688395ccf9b45acec34195e29b19038189fa
LeonardoRamos <leoslramos@gmail.com>
2021-06-03 15:21:27 -0300

    Refactor

--- src/app/main.js
+++ src/app/main.js
@@ -1,13 +1,10 @@
 import '@babel/polyfill';
 import config from './config/config';
 import app from './config/express';
-import db from './config/sequelize';
-
-const debug = require('debug')('node-generic-rest-api:main');
 
 Promise = require('bluebird'); 
 
-if (!module.parent) {
+if (!module.children) {
     app.listen(config.port, () => {
         console.info(`server started on port ${config.port} (${config.env})`);
     });
